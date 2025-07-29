bb2a8cf2b1154d9efedb67b11a64bb8b3f90d1ac
Nathan <an.email.com>
2025-07-29 17:42:11 +0100

    bugfix

--- src/files/clean.ts
+++ src/files/clean.ts
@@ -1,6 +1,5 @@
 import * as path from 'path';
-import { promisify } from 'util';
-import rimraf from 'rimraf';
+import { rimraf } from 'rimraf';
 import { AllOptions } from '../options';
 
 export const clean = async (options: AllOptions): Promise<void> => {
@@ -9,5 +8,5 @@ export const clean = async (options: AllOptions): Promise<void> => {
   }
   const cwd = options.files.cwd || process.cwd();
   const absoluteDir = path.resolve(cwd, options.files.destination.dir);
-  await promisify(rimraf)(absoluteDir);
+  await rimraf(absoluteDir);
 };
